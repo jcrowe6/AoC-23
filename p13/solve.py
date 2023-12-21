0d9b34089d4209d572e6a8c3316cf0421d9630ba
Jeremiah Crowell <jcrowe6@illinois.edu>
2023-12-21 13:55:14 -0600

    p13 day 1

--- p13/solve.py
+++ p13/solve.py
@@ -38,20 +38,43 @@ with open("input.txt") as f:
     while True:
         if process_map(f) == 0: break
 
+def match_right(nums,stack):
+    return stack == nums[-len(stack):]
+
+def match_left(nums,stack):
+    return stack == nums[:len(stack)]
+
+# checks if the line of reflection lies after the ith row/colum
+def reflects_over(arr, i):
+    a = i
+    b = i+1
+    while a >= 0 and b < len(arr):
+        if arr[a] != arr[b]: return False
+        a -= 1
+        b += 1
+    return True 
+
+# initially did very simple case of finding a single matching adjacent pair
+# then did more complex idea of using a stack and popping off the top when there were matches.. couldn't get it to work.
+# at this point, iterative search seems fine
 def find_reflect_i(map_):
-    for i in range(len(map_)-1): # check for row-reflection
-        if np.array_equal(map_[i,:],map_[i+1,:]):
+    rows,cols = map_to_nums(map_)
+    for i in range(len(rows)-1):
+        if reflects_over(rows, i):
             print("reflect after row",i+1)
             return 100*(i+1)
-    for j in range(len(map_[0])-1): # check for col-reflection
-        if np.array_equal(map_[:,j],map_[:,j+1]):
+    for j in range(len(cols)-1):
+        if reflects_over(cols, j):
             print("reflect after col",j+1)
             return j+1
+            
+    return "error!"
+
 
 
 total = 0
 for map_ in maps:
     print(map_)
     total += find_reflect_i(map_)
-    input()
+    #input()
 print(total)
